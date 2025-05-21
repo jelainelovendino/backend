0a426535baaabc3fc6dacf225ee07268b944b4a6
lyramae-05 <lyramaecoranes@gmail.com>
2025-05-21 13:29:09 +0800

    cors

--- config/cors.php
+++ config/cors.php
@@ -14,9 +14,9 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('front-end-eight-roan.vercel.app', 'http://localhost:3000')],
 
     'allowed_origins_patterns' => [],
 
